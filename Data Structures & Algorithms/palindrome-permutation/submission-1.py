abbfd99ce4f7e1d316f5b5852ceaffd4ec7c6623
Sathya Subramanian <pasupatirajesh@gmail.com>
2026-07-21 22:17:23 -0500

    Add: palindrome-permutation - submission-1

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        count = Counter(s)
+        odd_count = 0
+        for char, freq in count.items():
+            if freq %2 != 0:
+                odd_count+=1
+        
+        return odd_count <=1
+         
+            
