82b18087603180e4c66b306900978dc0a933545f
Sathya Subramanian <pasupatirajesh@gmail.com>
2026-07-24 13:21:25 -0500

    Add: minimum-number-of-moves-to-seat-everyone - submission-3

---
+++ Data Structures & Algorithms/minimum-number-of-moves-to-seat-everyone/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        max_index = max(max(seats), max(students)) + 1
+        count_seats = [0] * max_index
+        count_stu = [0] * max_index
+
+        i, j = 0, 0
+        res = 0
+        remain = len(seats)
+        for seat in seats:
+            count_seats[seat]+=1
+        for stu in students:
+            count_stu[stu] += 1
+        while remain:
+            while count_seats[i] == 0:
+                i+=1
+            while count_stu[j] == 0:
+                j+=1
+            
+            res+= abs(i-j)
+            count_seats[i] -=1
+            count_stu[j]-=1
+            remain -=1
+        return res 
+        
