334a120e29f1228ac879f3cc33f06e57e547a788
Sathya Subramanian <pasupatirajesh@gmail.com>
2026-07-23 22:48:38 -0500

    Add: palindrome-linked-list - submission-2

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-2.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        def rev_list(head):
+            prev = None
+            curr = head
+            while curr:
+                next_temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_temp
+            return prev
+        
+        left = head
+        right = rev_list(slow)
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+            
